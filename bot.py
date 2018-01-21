97b45713423d4d5463bcb948944f27b559118ecd
unknown <egorka.k2009@ukr.net>
2018-01-21 19:04:44 +0200

    fix repeating sending

--- bot.py
+++ bot.py
@@ -14,12 +14,12 @@ class Bot:
     def update(self, json_string):
         data = json.loads(json_string)
         print(data['message']['chat']['username'],':',data['message']['text'])
-        if data['message']['chat']['id'] in self.users and data['message']['chat']['message_id'] != self.users[data['message']['chat']['id']].get_last():
-            users[data['message']['chat']['id']].set_last(data['message']['chat']['message_id'])
+        if data['message']['chat']['id'] in self.users and data['message']['message_id'] != self.users[data['message']['chat']['id']].get_last():
+            users[data['message']['chat']['id']].set_last(data['message']['message_id'])
             if data['message']['text'] == '/start':
                 self.send_msg(data['message']['chat']['id'], 'Welcome:)')
         elif data['message']['chat']['id'] not in self.users:
-            self.user[data['message']['chat']['id']] = User(data['message']['chat']['message_id'])
+            self.user[data['message']['chat']['id']] = User(data['message']['message_id'])
             if data['message']['text'] == '/start':
                 self.send_msg(data['message']['chat']['id'], 'Welcome:)')
         
