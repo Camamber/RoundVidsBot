f3cd5d8605f020cc53a289531e3ff7284d92602b
unknown <egorka.k2009@ukr.net>
2018-01-21 12:40:06 +0200

    ready to json

--- app.py
+++ app.py
@@ -14,7 +14,16 @@ WEBHOOK_URL_PATH = "/%s/" % (API_TOKEN)
 class WebhookServer(object):
     @cherrypy.expose
     def index(self):
-        return '<h1>Xyi</h1>'
+        if 'content-length' in cherrypy.request.headers and \
+          'content-type' in cherrypy.request.headers and \
+            cherrypy.request.headers['content-type'] == 'application/json':
+            length = int(cherrypy.request.headers['content-length'])
+            print(length)
+            json_string = cherrypy.request.body.read(length)
+            print(json_string)
+            return json_string
+        else:
+            return '<h1>Xyi</h1>'
 
 config = {
     'global': {
