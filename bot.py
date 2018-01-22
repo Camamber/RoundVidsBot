a532269d952028e1649aef17a3833d2674de2473
unknown <egorka.k2009@ukr.net>
2018-01-22 17:21:05 +0200

    UPD

--- bot.py
+++ bot.py
@@ -3,8 +3,9 @@ import json
 import requests
 
 class Bot:
-    token = '543482719:AAGSzOTxA8AEoYIU8h8IAVfAlTHWbLLkRb0'
-    url = 'https://api.telegram.org/bot{0}/{1}'
+    TOKEN = '543482719:AAGSzOTxA8AEoYIU8h8IAVfAlTHWbLLkRb0'
+    URL = 'https://api.telegram.org/bot{0}/{1}'
+
     state= False
     users = {};
 
@@ -13,17 +14,32 @@ class Bot:
 
     def update(self, json_string):
         data = json.loads(json_string)
-        print(data['message']['chat']['username'],':',data['message']['text'])
+        print(data['message']['chat']['username'],':',data['message'])
         if data['message']['chat']['id'] in self.users:
-            self.exec_command(self.users[data['message']['chat']['id']], data['message']['text'])
+            self.exec_command(self.users[data['message']['chat']['id']], data['message'])
         else:
             self.new_user(data)         
 
     def send_msg(self, chat, text):
         params = {'chat_id': chat, 'text': text}
-        response = requests.post(self.url.format(self.token,'sendMessage'), data=params)
+        response = requests.post(self.url.format(self.TOKEN,'sendMessage'), data=params)
         return response
 
+
+
+    def exec_command(self, user, command):
+        if 'document' not in command:
+            if user.state == 'token_adding':
+                self.add_token(user, command['text'])
+            elif user.state == 'channel_adding':
+                self.add_channel(user, command['text'])
+            elif user.state == 'sleep':
+                self.sleep(user, command['text'])
+        elif 'document' in command:
+            if user.state == 'video_adding':
+                self.add_video(command['document')
+            
+            
     def new_user(self, data):
         if data['message']['text'] == '/start':
             self.users[data['message']['chat']['id']] = User(data['message']['chat']['id'])
@@ -31,16 +47,38 @@ class Bot:
         else:
             self.send_msg(data['message']['chat']['id'], 'Idk who are you man. Try /start to config me:)')
 
-    def exec_command(self, user, command):
-        if user.state == 'token':
-            if self.check_token(command):
-                user.token=command
-            else:
-                self.send_msg(user._id, 'Incorrect token. Try again')
+    def add_token(self, user, token):
+        response = requests.post(self.url.format(token, 'getMe'))
+        if response.json()['ok']:
+            user.token=token
+            user.state='channel_adding'
+            self.send_msg(user._id, 'Token successfully installed.')
+        else:
+            self.send_msg(user._id, 'Incorrect token. Try again.')
 
-    def check_token(self, token):
-        print(token)
-        response = requests.post(self.url.format(token,'getMe'))
-        return response.json()['ok']
+    def add_channel(self, user, channel):
+        params = {'chat_id': channel}
+        response = requests.post(self.url.format(user.token, 'getChat'), params)
+        if response.json()['ok']:
+            user.add_channel(channel)
+            user.state='working'
+            self.send_msg(user._id, 'Chat successfully added.')
+        else:
+            self.send_msg(user._id, 'There is no such chat or your bot are not in it.')
+
+    def sleep(self, user, command):
+        if command == '/token':
+            user.state'token_adding'
+        elif command == '/channels':
+            user.state='channel_adding'
+
+
+    def add_video(self, user, document):
+        if document['type'] == 'video/mp4':
+            self.send_msg(user._id, 'Nice Shoot')
+        else:
+            self.send_msg(user._id, 'Hey whait a minute it isnt video')
+                               
+            
+            
                 
-        
--- user.py
+++ user.py
@@ -2,8 +2,12 @@ class User:
     state=''
     _id=''
     token=''
+    channels=[]
     
     def __init__(self, _id):
-        self.state = 'token'
+        self.state = 'token_adding'
         self._id=_id
-        
+
+    def add_channel(self, channel):
+        if channel not in self.channels:
+            self.channels.append(channel)
