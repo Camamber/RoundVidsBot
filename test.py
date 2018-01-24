dbaf190c7e100634f2bbb2f4697013edabb8a988
unknown <egorka.k2009@ukr.net>
2018-01-24 13:49:44 +0200

    Working on button

--- test.py
+++ test.py
@@ -6,9 +6,9 @@ TOKEN = '456472998:AAGuE397SZVFgX5JIV022BFe6XQzzdDn_7Q'
 URL = 'https://api.telegram.org/bot{0}/{1}'
 
 file={'video_note': open('1.mp4', 'rb')}
-#keyboard = '{"inline_keyboard":[[{"text":"xyi","url":"pleshka.com"}]]}'
-keyboard = {'inline_keyboard':[{'text':'xyi','url':'pleshka.com'}]}
-params = {'text':'xyi','chat_id': '339018008', 'reply_markup':str(keyboard).replace('\'', '"') }
+#keyboard = '{"inline_keyboard":[[{"text":"xyi","url":"pleshka.com"}]]}'str(keyboard).replace('\'', '"')
+keyboard = {'inline_keyboard':[[{'text':'xyi','url':'pleshka.com'}]]}
+params = {'text':'xyi','chat_id': '339018008', 'reply_markup':'{"remove_keyboard":true}' }
 print(params)
 response = requests.post(URL.format(TOKEN,'sendVideoNote'), data=params, files=file)
 print(response.json())
